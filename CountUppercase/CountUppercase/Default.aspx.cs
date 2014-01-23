6fdc4ef58f6ed9ad6d4b2d9df9c0895160b781f0
HenrikGabrielsson <hg222aa@student.lnu.se>
2014-01-23 17:02:37 +0100

    Skapat formulär
    Formuläret är skapat. Det ger tillbaka korrekt antal versaler.

---
+++ CountUppercase/CountUppercase/Default.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CountUppercase
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void CountButton_Click(object sender, EventArgs e)
+        {
+            String inputString = UpperCaseTextBox.Text.ToString();
+
+            int uppercaseCount = 0;
+
+            foreach(char character in inputString)
+            {
+                int charCode = (int)character;
+
+                if (charCode >= 65 && charCode <= 90 || charCode == 196 || charCode == 197 || charCode == 214)
+                {
+                    uppercaseCount++; 
+                }
+   
+            }
+            UpperCaseTextBox.Enabled = false;
+
+            Result.Text = uppercaseCount.ToString();
+        }
+    }
+}
